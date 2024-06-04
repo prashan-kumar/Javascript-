a4d6e7353cbd4e726f60be1cf088f9ba6406150f
Prashant sharma <96737062+prashan-kumar@users.noreply.github.com>
2024-06-04 12:00:20 +0000

    string

---
+++ 1_basics/string.js
@@ -0,0 +1,42 @@
+const name = "prashant"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Prashant -hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   prashant    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://prashant.com/prashant%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+// let name='prashant';
+// const other1=name.substring(-1,3);
+// console.log(other1);
+// const other2=name.slice(-8,5);
+// console.log(other2);
