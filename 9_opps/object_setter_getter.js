806eba5ca547b03baeb136b60f51a51726a3d5db
Prashant sharma <96737062+prashan-kumar@users.noreply.github.com>
2024-06-25 13:16:02 +0000

    concept

---
+++ 9_opps/object_setter_getter.js
@@ -0,0 +1,36 @@
+// const User = {
+//     _email: 'h@hc.com',
+//     _password: "abc",
+
+
+//     get email(){
+//         return this._email.toUpperCase()
+//     },
+
+//     set email(value){
+//         this._email = value
+//     }
+// }
+
+// const tea = Object.create(User)
+// console.log(tea.email);
+
+
+
+
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
